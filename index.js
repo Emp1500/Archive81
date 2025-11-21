062c11c5a1a52a4443049493e1d3f3e5063d4c59
Emp1500 <vedantwagh539@gmail.com>
2025-11-21 11:41:30 +0530

    fix: use absolute paths for views and public dirs

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "views"));
+
+app.use(express.static(join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
